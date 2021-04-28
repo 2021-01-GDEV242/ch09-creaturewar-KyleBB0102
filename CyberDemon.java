0b0d2423b4223f497d39f00b56d4510cc98aee67
KyleBB0102 <G00283711@stu.raritanval.edu>
2021-04-28 19:36:13 -0400

    Create CyberDemon.java

---
+++ CyberDemon.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class CyberDemon here.
+ *
+ * @author Kyle Balao
+ * @version 04/28/21
+ */
+public class CyberDemon extends Demon
+{
+
+    /**
+     * Constructor for objects of class CyberDemon
+     */
+    public CyberDemon()
+    {
+        super();
+          
+    }
+    
+    
+}
